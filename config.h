33f0c4ec9081487ba7bbe71b40a66b0517657a06
Berkay Çubuk <berkay@berkaycubuk.com>
2021-07-30 16:08:47 +0300

    Fonts 2 patch applied

--- config.h
+++ config.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+
+static char *font2[] = {
+    "Symbola:pixelsize=15:antialias=true:autohint=true" // Unicode
+};
+
 static int borderpx = 2;
 
 /*
